a7bbfd018002dbbaddde7c1c055c9c52b1b8b012
Jana.S <jana.smolna@gmail.com>
2022-12-16 12:59:05 +0100

    hurá favicon

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     // Zkopírovat css/ to _site/css/
     eleventyConfig.addPassthroughCopy("css");
+    // favicons
+    eleventyConfig.addPassthroughCopy("icons");
     return {
         // možné formáty šablon
         templateFormats: ["njk", "html", "md"],
